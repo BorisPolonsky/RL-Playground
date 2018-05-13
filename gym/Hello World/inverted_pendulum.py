e4b93833d7e4c7f72143a3391054e18460177d19
Boris Polonsky <qdogan@163.com>
2018-05-13 23:54:24 +0800

    Update Network with Mutiple Layer Support

--- gym/Hello World/inverted_pendulum.py
+++ gym/Hello World/inverted_pendulum.py
@@ -2,12 +2,15 @@ import gym
 import tensorflow as tf
 import random
 import numpy as np
+
 env = gym.make("CartPole-v0")
-hidden_dim = 1000
+hidden_dim = 100
 lr = 1e-3
+n_hidden_layer = 5
 with tf.variable_scope("DQN-Model"):
-    net_input = tf.placeholder(shape=[None, 4], dtype=tf.float32)
-    net_output = tf.layers.dense(inputs=net_input, units=hidden_dim, activation=tf.nn.relu, use_bias=True)
+    net_input = net_output = tf.placeholder(shape=[None, 4], dtype=tf.float32)
+    for layer_i in range(n_hidden_layer):
+        net_output = tf.layers.dense(inputs=net_output, units=hidden_dim, activation=tf.nn.relu, use_bias=True)
     net_output = tf.layers.dense(inputs=net_output, units=2, activation=tf.nn.relu, use_bias=True)
 with tf.variable_scope("training-config"):
     target_output = tf.placeholder(shape=[None, 2], dtype=tf.float32)
@@ -18,7 +21,7 @@ with tf.variable_scope("training-config"):
     optimize_op = optimizer.minimize(loss, global_step=global_step)
 
 num_of_episode = 1000
-exploitation_rate = 0.1
+exploitation_rate = 0.3
 exploration_rate_decay = 1 - 1e-3
 gamma = 0.8  # Discount Factor
 with tf.Session() as sess:
@@ -27,6 +30,7 @@ with tf.Session() as sess:
     for i_episode in range(num_of_episode):
         observation = prev_observation = env.reset()
         num_of_steps = 0
+        time_stamps = [0]
         print("Episode {} begins with exploitation rate: {}.".format(i_episode, exploitation_rate))
         while True:
             env.render()
@@ -38,7 +42,8 @@ with tf.Session() as sess:
             prev_observation = observation
             observation, reward, done, info = env.step(action)
             if done:
-                q_vals[0][action] = reward
+                # q_vals[0][action] = reward
+                q_vals[0][action] = -20  # Override the default reward
             else:
                 q_vals[0][action] = reward + max(sess.run(net_output, feed_dict={net_input: observation[np.newaxis, :]})[0])
             sess.run(optimize_op, feed_dict={net_input: prev_observation[np.newaxis, :],
