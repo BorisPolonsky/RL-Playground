103613d46ee2cfa054f3eb2c23c7526632a1a27b
Polonsky <qdogan@163.com>
2018-05-17 18:41:51 +0800

    Update
    Added experience replay.
    Added configuration of maximum exploitation rate.
    Reduced number of layers and removed activation of output layer.

--- gym/Hello World/inverted_pendulum.py
+++ gym/Hello World/inverted_pendulum.py
@@ -5,15 +5,15 @@ import numpy as np
 
 env = gym.make("CartPole-v0")
 hidden_dim = 100
-lr = 1e-3
-n_hidden_layer = 5
+lr = 1e-4
+n_hidden_layer = 1
 # This network takes the 4-dimensional observation of the environment as state input, and outputs a 2-dimensional vector
 # representing the Q-value for taking action 0 or 1, respectively.
 with tf.variable_scope("DQN-Model"):
     net_input = net_output = tf.placeholder(shape=[None, 4], dtype=tf.float32)
     for layer_i in range(n_hidden_layer):
         net_output = tf.layers.dense(inputs=net_output, units=hidden_dim, activation=tf.nn.relu, use_bias=True)
-    net_output = tf.layers.dense(inputs=net_output, units=2, activation=tf.nn.relu, use_bias=True)
+    net_output = tf.layers.dense(inputs=net_output, units=2, use_bias=True)
 with tf.variable_scope("training-config"):
     target_output = tf.placeholder(shape=[None, 2], dtype=tf.float32)
     loss = tf.reduce_mean((target_output - net_output)**2)
@@ -23,12 +23,18 @@ with tf.variable_scope("training-config"):
     optimize_op = optimizer.minimize(loss, global_step=global_step)
 
 num_of_episode = 3000
-exploitation_rate = 0.3
+starting_exploitation_rate = 0.3
+maximum_exploitation_rate = 0.9
 exploration_rate_decay = 1 - 1e-3
 gamma = 0.8  # Discount Factor
+batch_size = 50
+max_memory_size = 10000
+
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     sess.run(tf.assign(learning_rate, lr))
+    memory = []  # (state, action, reward, next_state)
+    exploitation_rate = starting_exploitation_rate
     for i_episode in range(num_of_episode):
         observation = prev_observation = env.reset()
         num_of_steps = 0
@@ -36,22 +42,34 @@ with tf.Session() as sess:
         print("Episode {} begins with exploitation rate: {}.".format(i_episode, exploitation_rate))
         while True:
             env.render()
-            q_vals = sess.run(net_output, feed_dict={net_input: observation[np.newaxis, :]})
             if random.random() < exploitation_rate:
+                q_vals = sess.run(net_output, feed_dict={net_input: observation[np.newaxis, :]})
                 action = np.argmax(q_vals[0])  # Exploitation
             else:
                 action = env.action_space.sample()  # Exploration
             prev_observation = observation
             observation, reward, done, info = env.step(action)
-            if done:
-                # q_vals[0][action] = reward
-                q_vals[0][action] = -20  # Override the default reward
-            else:
-                q_vals[0][action] = reward + gamma * max(sess.run(net_output, feed_dict={net_input: observation[np.newaxis, :]})[0])
-            sess.run(optimize_op, feed_dict={net_input: prev_observation[np.newaxis, :],
-                                             target_output: q_vals})
+            memory.append((prev_observation, action, reward, (observation if not done else None)))
+            if len(memory) > max_memory_size:
+                memory.pop(0)
+
+            batch = random.sample(memory, batch_size if batch_size <= len(memory) else len(memory))
+            states = np.array([mem[0] for mem in batch])
+            next_states = np.array([mem[3] if mem[3] is not None else np.zeros(env.observation_space.shape[0]
+)for mem in batch])
+            max_q_val_next = np.max(sess.run(net_output, feed_dict={net_input: next_states}), axis=1)
+            target_q_val = sess.run(net_output, feed_dict={net_input: states})
+            for i_sample, (_, current_action, current_reward, next_state) in enumerate(batch):
+                if next_state is None:
+                    target_q_val[i_sample][current_action] = current_reward
+                else:
+                    target_q_val[i_sample][current_action] = current_reward + gamma * max_q_val_next[i_sample]
+            sess.run(optimize_op, feed_dict={net_input: states,
+                                             target_output: target_q_val})
             num_of_steps += 1
             if done:
                 print("Episode {} ended after {} step(s).".format(i_episode, num_of_steps))
                 exploitation_rate = 1 - (1 - exploitation_rate) * exploration_rate_decay
+                if maximum_exploitation_rate is not None and exploitation_rate > maximum_exploitation_rate:
+                    exploitation_rate = maximum_exploitation_rate
                 break
